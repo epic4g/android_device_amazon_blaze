311508081aad8b26a06207f123c8826263d19c9f
JackpotClavin <JonClavin@gmail.com>
2011-12-08 19:05:45 -0500

    Update device_blaze.mk

--- device_blaze.mk
+++ device_blaze.mk
@@ -8,13 +8,17 @@ $(call inherit-product-if-exists, vendor/amazon/blaze/blaze-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += device/amazon/blaze/overlay
 
 PRODUCT_COPY_FILES += \
-    device/amazon/blaze/root/init.rc:root/init.rc \
     device/amazon/blaze/root/init.omap4430.rc:root/init.omap4430.rc \
     device/amazon/blaze/root/ueventd.omap4430.rc:root/ueventd.omap4430.rc \
     device/amazon/blaze/root/ueventd.rc:root/ueventd.rc \
+    device/amazon/blaze/root/init.rc:root/init.rc \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/misc/vold.fstab:system/etc/vold.fstab
+
+# Media Profile
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/misc/media_profiles.xml:system/etc/media_profiles.xml
 
 PRODUCT_LOCALES += mdpi
 
@@ -29,28 +33,43 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
     frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
-    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    device/amazon/blaze/misc/TICameraCameraProperties.xml:system/etc/TICameraCameraProperties.xml \
+
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
 PRODUCT_PACKAGES += \
     librs_jni \
+    hwcomposer.omap4 \
+    ti_omap4_ducati_bins \
+    libtimemmgr \
+    CameraOMAP4 \
+    Camera \
+    camera_test \
     tiwlan.ini \
-    dspexec \
-    libbridge \
     wlan_cu \
-    libtiOsLib \
     wlan_loader \
     libCustomWifi \
     alsa.omap4 \
     wpa_supplicant.conf \
     dhcpcd.conf \
-    libLCML \
-    libVendor_ti_omx \
-    Usb
+    hwcomposer.default \
+    hwcomposer.omap4 \
+    Usb \
+    boardidentity \
+    libboardidentity \
+    libboard_idJNI \
+    Board_id \
 
+PRODUCT_PACKAGES := \
+    ti_omap4_ducati_bins \
+    libOMX_Core \
+    libOMX.TI.DUCATI1.VIDEO.DECODER \
 
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.opengles.version=131072
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := device/amazon/blaze/kernel
@@ -71,9 +90,16 @@ ro.ril.gprsclass=10 \
 ro.config.notification_sound=ro.config.alarm_alert=Alarm_Classic.ogg \
 net.bt.name=Android \
 dalvik.vm.stack-trace-file=/data/anr/traces.txt \
+ro.opengles.version=131072 \
+net.dns1=8.8.8.8 \
+net.dns2=8.8.4.4 \
+hwui.render_dirty_regions=false \
 
 $(call inherit-product, build/target/product/full_base.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_blaze
+PRODUCT_NAME := cm_blaze
 PRODUCT_DEVICE := blaze
+
+$(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
+$(call inherit-product-if-exists, vendor/cm/config/common_full_tablet_wifionly.mk)
